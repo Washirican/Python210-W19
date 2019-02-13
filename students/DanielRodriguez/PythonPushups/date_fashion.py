7dfbd0932d5078af6c86f9276cd989f6fa16ea4e
Washirican <danrod@uw.edu>
2019-02-12 23:36:56 -0800

    Python Pushups

--- students/DanielRodriguez/PythonPushups/date_fashion.py
+++ students/DanielRodriguez/PythonPushups/date_fashion.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------#
-# Title:
+# Title: Date Fashion Push up
 # Change Log: (Who, When, What)
 # D. Rodriguez, 2019-01-19, Revision notes
 # ----------------------------------------------------------------#
@@ -17,16 +17,18 @@
 # date_fashion(5, 2) → 0
 # date_fashion(5, 5) → 1
 
-# -- Data --#
-# declare variables and constants
 
 # TODO: In work...
-
-
 # -- Processing --#
 def date_fashion(you, date):
-    None
+    if you <= 2 or date <= 2:
+        return 0
+    elif you >= 8 or date >= 8:
+        return 2
+    else:
+        return 1
 
 
-# -- Presentation (Input/Output) --#
-print(date_fashion(5, 10))
+print(date_fashion(5, 10) == 2)
+print(date_fashion(5, 2) == 0)
+print(date_fashion(5, 5) == 1)
